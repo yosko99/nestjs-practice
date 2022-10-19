2d829124b57040d932e80f91645acf6988379033
yosko99 <thedriftmaniaka1997@gmail.com>
2022-10-19 13:36:34 +0300

    Add http error filter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpErrorFilter } from './filters/httpError.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpErrorFilter());
   await app.listen(3000);
 }
 bootstrap();
